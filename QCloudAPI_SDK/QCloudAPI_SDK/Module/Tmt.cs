6224918ea7c6035e21f01781376ccb26937edb7e
jx wang <jx@x2v3.com>
2018-06-26 10:07:13 +0800

    add  tmt module

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Tmt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    public class Tmt : Base
+    {
+        public Tmt()
+        {
+            serverHost = "tmt.tencentcloudapi.com";
+        }
+    }
+}
