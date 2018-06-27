bfbc75fecbb0901d34c990a88f816cfa3ef2749a
jx wang <jx@x2v3.com>
2018-06-27 17:30:42 +0800

    add image recognition

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/ImageRecognition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    public class ImageRecognition : BaseV20180321
+    {
+        public ImageRecognition()
+        {
+            serverHost = "recognition.image.myqcloud.com";
+        }
+    }
+
+    public class GeneralImageRecognition : ImageRecognition
+    {
+        public GeneralImageRecognition()
+        {
+            serverUri = "/ocr/general";
+        }
+    }
+}
